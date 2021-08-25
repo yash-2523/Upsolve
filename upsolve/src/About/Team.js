6c3b434b9dfd15bb0f645136b2ba5704d358f8cd
yash-2523 <56871771+yash-2523@users.noreply.github.com>
2021-08-25 11:38:13 +0530

    team members updated

--- upsolve/src/About/Team.js
+++ upsolve/src/About/Team.js
@@ -5,9 +5,9 @@ export default function Team() {
     const [descnum,setdescNum] = useState(0);
     let description = [];
     description.push(["Jay Mehta","orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]);
-    description.push(["Shivam Thakker","orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]);
-    description.push(["Parshwa Shah","orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]);
-    description.push(["Yash Doshi","orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]);
+    description.push(["Shivam Thakker","Our 3D model wizard, Shivam Thakker, always on the search for the perfect!🔥."]);
+    description.push(["Parshwa Shah","When he's not practicing backend development, Parshwa likes to ace on our project's backend development!!✨."]);
+    description.push(["Yash Doshi","Dreams have only one master and our Upsolve galaxy has one and only one UI/UX master, Yash Doshi! 🤩."]);
     
 
     return (
@@ -20,10 +20,10 @@ export default function Team() {
                 </div>
                 <div className="mx-auto position-relative team-ring">
                     
-                    <div className={"icon" + (descnum === 1 ? " icon-active" :  "")} onClick={() => setdescNum(1)} style={{top: '0%', left: "50%", transform: 'translate(-50%,-50%)'}}><img className="icon1" src={'https://source.unsplash.com/random'}></img></div>
-                    <div className={"icon" + (descnum === 2 ? " icon-active" :  "")} onClick={() => setdescNum(2)} style={{top: '50%', left: "0%", transform: 'translate(-50%,-50%)'}}><img className="icon1" src={'https://source.unsplash.com/random'}></img></div>
-                    <div className={"icon" + (descnum === 3 ? " icon-active" :  "")} onClick={() => setdescNum(3)} style={{top: '100%', left: "50%", transform: 'translate(-50%,-50%)'}}><img className="icon1" src={'https://source.unsplash.com/random'}></img></div>
-                    <div className={"icon" + (descnum === 4 ? " icon-active" :  "")} onClick={() => setdescNum(4)} style={{top: '50%', left: "100%", transform: 'translate(-50%,-50%)'}}><img className="icon1" src={'https://source.unsplash.com/random'}></img></div>
+                    <div className={"icon" + (descnum === 1 ? " icon-active" :  "")} onClick={() => setdescNum(1)} style={{top: '0%', left: "50%", transform: 'translate(-50%,-50%)'}}><img className="icon1" src={'jay.jpeg'}></img></div>
+                    <div className={"icon" + (descnum === 2 ? " icon-active" :  "")} onClick={() => setdescNum(2)} style={{top: '50%', left: "0%", transform: 'translate(-50%,-50%)'}}><img className="icon1" src={'shivam.jpeg'}></img></div>
+                    <div className={"icon" + (descnum === 3 ? " icon-active" :  "")} onClick={() => setdescNum(3)} style={{top: '100%', left: "50%", transform: 'translate(-50%,-50%)'}}><img className="icon1" src={'parshwa.jpeg'}></img></div>
+                    <div className={"icon" + (descnum === 4 ? " icon-active" :  "")} onClick={() => setdescNum(4)} style={{top: '50%', left: "100%", transform: 'translate(-50%,-50%)'}}><img className="icon1" src={'yash.jpeg'}></img></div>
                 </div>
             </div>
         </section>
