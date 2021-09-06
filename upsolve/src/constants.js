1efb0c1345f6acc7c0f9f904180b074c35e32e20
yash-2523 <56871771+yash-2523@users.noreply.github.com>
2021-09-06 17:42:36 +0530

    azure deploy

--- upsolve/src/constants.js
+++ upsolve/src/constants.js
@@ -1,8 +1,8 @@
 let dev = {
-    url: "http://20.198.103.171:4000",
+    url: "https://20.198.103.171:4000",
 }
 let prod = {
-    url: "http://20.198.103.171:4000",
+    url: "https://20.198.103.171:4000",
 }
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
