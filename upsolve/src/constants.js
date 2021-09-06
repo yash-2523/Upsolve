babfa8419d8d2c1488a92d7dd6f0f8adc5d51115
yash-2523 <56871771+yash-2523@users.noreply.github.com>
2021-09-06 22:45:58 +0530

    New update

--- upsolve/src/constants.js
+++ upsolve/src/constants.js
@@ -1,8 +1,8 @@
 let dev = {
-    url: "https://20.198.103.171:4000",
+    url: "https://20.198.103.171",
 }
 let prod = {
-    url: "https://20.198.103.171:4000",
+    url: "https://20.198.103.171",
 }
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
